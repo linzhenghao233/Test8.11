890b588f5128d62cf91c8b969df4ec4caea99d0e
林政浩 <1392872316@qq.com>
2023-02-26 23:37:57 +0800

    0226

--- Test8_11.c
+++ Test8_11.c
@@ -126,3 +126,155 @@
 //}
 
 //6.
+//char get_first(void) {
+//	int ch, first_word = 0;
+//
+//	ch = getchar();
+//	if (!isspace(ch))
+//		first_word = ch;
+//	while (getchar() != '\n')
+//		continue;
+//
+//	return first_word;
+//}
+
+//7.
+
+//8.
+int main(void) {
+	float num1, num2;
+	int ch;
+
+	printf("请输入你的选择：\n");
+	printf("a.加法     b.减法\n");
+	printf("c.乘法     d.除法\n");
+	printf("q.退出程序\n");
+	
+	while ((ch = getchar()) != 'q') {
+		if (ch == 'a') {
+			printf("输入第一个数字：");
+			while (!scanf_s("%f", &num1)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("输入第二个数字：");
+			while (!scanf_s("%f", &num2)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("%g + %g = %g\n", num1, num2, (double)num1 + num2);
+
+			printf("请再次输入你的选择：\n");
+			printf("a.加法     b.减法\n");
+			printf("c.乘法     d.除法\n");
+			printf("q.退出程序\n");
+
+			while ((ch = getchar()) != '\n')		//去除回车的影响
+				continue;
+		}
+		else if (ch == 'b') {
+			printf("输入第一个数字：");
+			while (!scanf_s("%f", &num1)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("输入第二个数字：");
+			while (!scanf_s("%f", &num2)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("%g - %g = %g\n", num1, num2, (double)num1 - num2);
+
+			printf("请再次输入你的选择：\n");
+			printf("a.加法     b.减法\n");
+			printf("c.乘法     d.除法\n");
+			printf("q.退出程序\n");
+
+			while ((ch = getchar()) != '\n')		//去除回车的影响
+				continue;
+		}
+		else if (ch == 'c') {
+			printf("输入第一个数字：");
+			while (!scanf_s("%f", &num1)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("输入第二个数字：");
+			while (!scanf_s("%f", &num2)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("%g * %g = %g\n", num1, num2, (double)num1 * num2);
+
+			printf("请再次输入你的选择：\n");
+			printf("a.加法     b.减法\n");
+			printf("c.乘法     d.除法\n");
+			printf("q.退出程序\n");
+
+			while ((ch = getchar()) != '\n')		//去除回车的影响
+				continue;
+		}
+		else if (ch == 'd') {
+			printf("输入第一个数字：");
+			while (!scanf_s("%f", &num1)) {
+				while ((ch = getchar()) != '\n')
+					putchar(ch);
+				printf("不是数字，请再次输入：");
+				continue;
+			}
+
+			printf("输入第二个数字：");
+			while (!scanf_s("%f", &num2) || num2 == 0) {
+				if (num2 == 0) {
+					printf("0不能作为除数，请再次输入：");
+					num2++;		//改变num2的值，使其不影响判断。
+				}	
+				else {
+					while ((ch = getchar()) != '\n')
+						putchar(ch);	
+					printf("不是数字，请再次输入：");
+					continue;
+				}
+			}
+
+			printf("%g / %g = %g\n", num1, num2, (double)num1 / num2);
+
+			printf("请再次输入你的选择：\n");
+			printf("a.加法     b.减法\n");
+			printf("c.乘法     d.除法\n");
+			printf("q.退出程序\n");
+
+			while ((ch = getchar()) != '\n')		//去除回车的影响
+				continue;
+		}
+		else {
+			printf("请输入正确的选项（a、b、c、d、q）:\n");
+			printf("请输入你的选择：\n");
+			printf("a.加法     b.减法\n");
+			printf("c.乘法     d.除法\n");
+			printf("q.退出程序\n");
+			continue;
+		}	
+	}
+	printf("结束。");
+
+	return 0;
+}
